544b0a0c18c954ca6226cd131a4acd293a3c07b7
Kihira Kota <kihira@puroya.jp>
2024-04-21 18:20:47 +0900

    #3 アプリケーションのバージョンのミドルウェアを作成

---
+++ tests/Unit/Middleware/AppVersionTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Unit\Middleware;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use PHPUnit\Framework\TestCase;
+use Proya\Laravel\Middleware\AppVersion;
+
+class AppVersionTest extends TestCase
+{
+    public function testSmoke()
+    {
+        $request = new Request();
+
+        $middleware = new AppVersion();
+        $response = $middleware->handle($request, fn() => new Response());
+
+        $this->assertEquals('1.0.0', $response->headers->get('App-Version'));
+    }
+}
